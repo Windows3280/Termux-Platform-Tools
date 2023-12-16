7e40f13a4df16ba5020fc32c83d769d5b36661cc
Windows3280 <110899603+Windows3280@users.noreply.github.com>
2023-12-16 20:23:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
-apt-get update
-apt-get  --assume-yes upgrade
-apt-get  --assume-yes install coreutils gnupg wget
+echo Updating pkg repositories
+pkg update
+echo Upgrading packages
+pkg upgrade -y
+echo Installing dependencies
+pkg  install coreutils gnupg wget -y
+echo Adding repository
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/rendiix.list" ]; then
   mkdir -p $PREFIX/etc/apt/sources.list.d
   echo -e "deb https://rendiix.github.io android-tools termux" > $PREFIX/etc/apt/sources.list.d/rendiix.list 
   wget -qP $PREFIX/etc/apt/trusted.gpg.d https://rendiix.github.io/rendiix.gpg 
-  apt update
-  apt install platform-tools
+  pkg update -y
+  pkg install platform-tools -y
 else 
-  echo "Repo already installed"
-  apt install platform-tools
+  echo "FAILED: repository already added. If you are trying to update ADB, use pkg install platform-tools."
+  pkg install platform-tools -y
 fi
 
-echo "done!"
+echo "Done! Use adb or fastboot."
